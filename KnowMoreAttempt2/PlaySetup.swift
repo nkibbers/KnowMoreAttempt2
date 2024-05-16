ac3ce7feca416dbe0e6ddf174051e43c42886dee
Nadia Kiblisky <nkiblisky@gmail.com>
2024-05-16 14:36:30 +1000

    added leaderboard and made titles more dynamic

--- KnowMoreAttempt2/PlaySetup.swift
+++ KnowMoreAttempt2/PlaySetup.swift
@@ -11,109 +11,119 @@ struct PlaySetup: View {
                 Image("whitebg")
                     .resizable()
                     .ignoresSafeArea()
-                Image("blackbg")
-                    .frame(width: 300, height: 200)
-                    .mask(VStack(spacing: -40) {
-                        Text("play")
-                            .font(Font.custom("Mont-HeavyDEMO", size: 85))
-                            .foregroundColor(.white)
-                            .kerning(-1)
-                    }
-                    )
-                    .position(x: 140, y: 130)
-                VStack {
-                    TextField("Enter your player name", text: $name)
-                        .foregroundColor(Color("KMBlack"))
-                        .padding(.horizontal, 20)
-                        .background(
-                            Image("lightbluebg")
-                                .resizable()
-                                .scaledToFill()
-                                .mask(
-                                    RoundedRectangle(cornerRadius: 0)
-                                        .frame(width:.infinity, height: 80))
-                        )
-                        .padding(.bottom, 20)
-                        .padding(.top, 300)
-                    LazyVGrid(columns: Array(repeating: GridItem(), count: 2), spacing: 0) {
-                        
-                        ZStack {
-                            Image("purplebg")
-                                .resizable()
-                                .frame(width: 200, height: 200)
-                            VStack {
-                                Image("greenbg")
-                                    .resizable()
-                                    .frame(width: 200, height: 40)
-                                    .mask(
-                                        Text("difficulty")
-                                            .font(Font.custom("Mont-HeavyDEMO", size: 38))
-                                            .foregroundColor(.white)
-                                            .kerning(-1)
-                                    )
-                                Picker(selection: $selectedDifficulty, label: Text("Difficulty"))
-                                {
-                                    Text("Easy").tag(1)
-                                    Text("Medium").tag(2)
-                                    Text("Difficult").tag(3)
-                                }
-                                .pickerStyle(.wheel)
-                                .frame(width: 175, height: 90)
-                                .foregroundColor(Color("KMBlack"))
-                            }
+                VStack (alignment: .leading) {
+                    Image("blackbg")
+                        .resizable()
+                        .frame(width: 200, height: 200)
+                        .mask {
+                            Text("play")
+                                .font(Font.custom("Mont-HeavyDEMO", size: 85))
+                                .foregroundColor(.white)
+                                .kerning(-1)
                         }
-                        
-                        Button(action: {
-                            isPlayButtonPressed.toggle()
-                        }) {
-                            ZStack{
-                                Image("yellowbg")
+                        .padding(.top, 50)
+                    
+                    VStack {
+                        TextField("Enter your player name", text: $name)
+                            .foregroundColor(Color("KMBlack"))
+                            .padding(.horizontal, 20)
+                            .background(
+                                Image("lightbluebg")
+                                    .resizable()
+                                    .scaledToFill()
+                                    .mask(
+                                        RoundedRectangle(cornerRadius: 0)
+                                            .frame(width:.infinity, height: 80))
+                            )
+                            .padding(.bottom, 20)
+
+                        LazyVGrid(columns: Array(repeating: GridItem(), count: 2), spacing: 0) {
+                            
+                            ZStack {
+                                Image("purplebg")
                                     .resizable()
                                     .frame(width: 200, height: 200)
-                                
+                                VStack {
+                                    Image("greenbg")
+                                        .resizable()
+                                        .frame(width: 200, height: 40)
+                                        .mask(
+                                            Text("difficulty")
+                                                .font(Font.custom("Mont-HeavyDEMO", size: 38))
+                                                .foregroundColor(.white)
+                                                .kerning(-1)
+                                        )
+                                    Picker(selection: $selectedDifficulty, label: Text("Difficulty"))
+                                    {
+                                        Text("Easy").tag(1)
+                                            .font(Font.custom("Mont-HeavyDemo", size: 17))
+                                            .foregroundColor(Color("KMBlack"))
+                                        Text("Medium").tag(2)
+                                            .font(Font.custom("Mont-HeavyDemo", size: 17))
+                                            .foregroundColor(Color("KMBlack"))
+                                        Text("Difficult").tag(3)
+                                            .font(Font.custom("Mont-HeavyDemo", size: 17))
+                                            .foregroundColor(Color("KMBlack"))
+                                    }
+                                    .pickerStyle(.wheel)
+                                    
+                                    .frame(width: 175, height: 90)
+                                    .foregroundColor(Color("KMBlack"))
+                                }
+                            }
+                            
+                            Button(action: {
+                                isPlayButtonPressed.toggle()
+                            }) {
+                                ZStack{
+                                    Image("yellowbg")
+                                        .resizable()
+                                        .frame(width: 200, height: 200)
+                                    
+                                    Image("orangebg")
+                                        .resizable()
+                                        .frame(width: 200, height: 200)
+                                        .mask(
+                                            Text("play")
+                                                .font(Font.custom("Mont-HeavyDEMO", size: 45))
+                                                .foregroundColor(.white)
+                                                .kerning(-1)
+                                        )
+                                }
+                            }
+                            
+                            ZStack{
+                                Image("lightbluebg")
+                                    .resizable()
+                                    .frame(width: 200, height: 200)
+                            }
+                            
+                            ZStack{
+                                Image("greenbg")
+                                    .resizable()
+                                    .frame(width: 200, height: 200)
+                            }
+                            
+                            ZStack{
                                 Image("orangebg")
                                     .resizable()
                                     .frame(width: 200, height: 200)
-                                    .mask(
-                                        Text("play")
-                                            .font(Font.custom("Mont-HeavyDEMO", size: 45))
-                                            .foregroundColor(.white)
-                                            .kerning(-1)
-                                    )
                             }
+                            ZStack{
+                                Image("brownbg")
+                                    .resizable()
+                                    .frame(width: 200, height: 200)
+                            }
+                            .navigationBarBackButtonHidden(true)
+                            .navigationBarItems(
+                                leading: Button(action: {
+                                    isPlayButtonPressed = false
+                                }) {
+                                    Image(systemName: "chevron.left")
+                                        .foregroundColor(Color("KMBlack"))
+                                }
+                            )
                         }
-                        
-                        ZStack{
-                            Image("lightbluebg")
-                                .resizable()
-                                .frame(width: 200, height: 200)
-                        }
-                        
-                        ZStack{
-                            Image("greenbg")
-                                .resizable()
-                                .frame(width: 200, height: 200)
-                        }
-                        
-                        ZStack{
-                            Image("orangebg")
-                                .resizable()
-                                .frame(width: 200, height: 200)
-                        }
-                        ZStack{
-                            Image("brownbg")
-                                .resizable()
-                                .frame(width: 200, height: 200)
-                        }
-                        .navigationBarBackButtonHidden(true)
-                                   .navigationBarItems(
-                                       leading: Button(action: {
-                                           isPlayButtonPressed = false
-                                       }) {
-                                           Image(systemName: "chevron.left")
-                                               .foregroundColor(Color("KMBlack"))
-                                       }
-                                   )
                     }
                 }
             }
--- KnowMoreAttempt2/leaderboard.swift
+++ KnowMoreAttempt2/leaderboard.swift
@@ -1,18 +1,96 @@
-//
-//  leaderboard.swift
-//  KnowMoreAttempt2
-//
-//  Created by Nadia Kiblisky Hauyon on 16/5/2024.
-//
-
 import SwiftUI
 
 struct leaderboard: View {
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ZStack {
+                Image("whitebg")
+                    .resizable()
+                    .ignoresSafeArea()
+                VStack (alignment: .leading) {
+                    Image("blackbg")
+                        .resizable()
+                        .frame(width: 300, height: 200)
+                        .mask(VStack(spacing: -40) {
+                            Text("leaderboard")
+                                .font(Font.custom("Mont-HeavyDEMO", size: 85))
+                                .kerning(-1)
+                        }
+                        )
+                    ScrollView {
+                        LazyVGrid(columns: Array(repeating: GridItem(), count: 1), spacing: 0) {
+                            
+                            ZStack {
+                                Image("purplebg")
+                                    .resizable()
+                                    .frame(width: .infinity, height: 100)
+                                VStack {
+                                    Image("greenbg")
+                                        .resizable()
+                                        .frame(width: 200, height: 40)
+                                        .mask(
+                                            Text("player1")
+                                                .font(Font.custom("Mont-HeavyDEMO", size: 38))
+                                                .foregroundColor(.white)
+                                                .kerning(-1)
+                                            
+                                        )
+                                }
+                            }
+                            
+                            ZStack{
+                                Image("yellowbg")
+                                    .resizable()
+                                    .frame(width: .infinity, height: 100)
+                                
+                                Image("orangebg")
+                                    .resizable()
+                                    .frame(width: 200, height: 100)
+                                    .mask(
+                                        Text("play")
+                                            .font(Font.custom("Mont-HeavyDEMO", size: 38))
+                                            .foregroundColor(.white)
+                                            .kerning(-1)
+                                    )
+                            }
+                            
+                            ZStack{
+                                Image("lightbluebg")
+                                    .resizable()
+                                    .frame(width: .infinity, height: 100)
+                            }
+                            
+                            ZStack{
+                                Image("greenbg")
+                                    .resizable()
+                                    .frame(width: .infinity, height: 100)
+                            }
+                            
+                            ZStack{
+                                Image("orangebg")
+                                    .resizable()
+                                    .frame(width: .infinity, height: 100)
+                            }
+                            ZStack{
+                                Image("brownbg")
+                                    .resizable()
+                                    .frame(width: .infinity, height: 100)
+                            }
+                                                    .navigationBarBackButtonHidden(true)
+                                                               .navigationBarItems(
+                                                                   leading: Button(action: {
+                                                                   }) {
+                                                                       Image(systemName: "chevron.left")
+                                                                           .foregroundColor(Color("KMBlack"))
+                                                                   }
+                                                               )
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
-#Preview {
-    leaderboard()
-}
+#Preview {leaderboard()}
