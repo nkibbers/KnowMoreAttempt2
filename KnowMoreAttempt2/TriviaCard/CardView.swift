551dcbab8e68db3a0d7233e76d00f99fb4279ff5
Rudra Parikh <rudraparikh6@gmail.com>
2024-05-16 14:31:19 +1000

    Made Trivia Question View, Crad Flip 70%

---
+++ KnowMoreAttempt2/TriviaCard/CardView.swift
@@ -0,0 +1,18 @@
+//
+//  CardView.swift
+//  KnowMoreAttempt2
+//
+//  Created by Rudra Parikh on 16/5/2024.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardView()
+}
