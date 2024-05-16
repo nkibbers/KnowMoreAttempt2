72965dc45710a4485fb30b124afcdc060318f876
Nadia Kiblisky <nkiblisky@gmail.com>
2024-05-16 14:35:52 +1000

    added leaderboad, made titles more dynamic

---
+++ KnowMoreAttempt2/leaderboard.swift
@@ -0,0 +1,18 @@
+//
+//  leaderboard.swift
+//  KnowMoreAttempt2
+//
+//  Created by Nadia Kiblisky Hauyon on 16/5/2024.
+//
+
+import SwiftUI
+
+struct leaderboard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    leaderboard()
+}
