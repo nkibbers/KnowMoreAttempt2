ad8381ce8d98c9bc75b318c7f3dc9bacfa439119
RobinTheHoo <ved.dave7@gmail.com>
2024-05-17 19:18:17 +1000

    Update archive view

--- KnowMoreAttempt2/Views/TriviaArchiveView.swift
+++ KnowMoreAttempt2/Views/TriviaArchiveView.swift
@@ -9,26 +9,62 @@ import SwiftUI
 
 struct TriviaArchiveView: View {
     @ObservedObject var quizzoManager: QuizzoManager
+    let colors = ["tangeringbg", "lightbluebg", "yellowbg", "purplebg", "orangebg"]
+    @State var index = 0
     
     var body: some View {
         NavigationView {
-            List(quizzoManager.playedQuestions, id: \.id) { question in
-                VStack(alignment: .leading, spacing: 10) {
-                    Text(question.formattedQuestion)
-                        .font(.headline)
-                    ForEach(question.answer) { answer in
-                        if (answer.isCorrect) {
-                            Text(answer.text)
-                                .font(.subheadline)
-//                                .foregroundColor(answer.isCorrect ? .green : .primary)
+            ZStack {
+                Image("whitebg")
+                    .resizable()
+                    .ignoresSafeArea()
+                VStack (alignment: .leading) {
+                    Image("blackbg")
+                        .resizable()
+                        .frame(width: 300, height: 100)
+                        .mask {
+                            Text("archive")
+                                .font(Font.custom("Mont-HeavyDEMO", size: 64))
+                                .foregroundColor(.white)
+                                .kerning(-1)
                         }
-                        
+                        .padding(.top, 50)
+                    
+                    ScrollView {
+                        ForEach(quizzoManager.playedQuestions, id: \.id) {question in
+                            VStack(alignment: .leading) {
+                                Text(question.formattedQuestion)
+                                    .font(.headline)
+                                    .multilineTextAlignment(.leading)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                    .padding()
+                                ForEach(question.answer) { answer in
+                                    if (answer.isCorrect) {
+                                        Text(answer.text)
+                                            .font(.subheadline)
+                                            .padding()
+                                    }
+                                }
+                            }
+                            .background(Image(assignColor()).resizable())
+                            .padding(.horizontal, 20.0)
+                        }
                     }
+                    .frame(width: .infinity)
                 }
-                .padding()
+//                .navigationTitle("Archive")
             }
-            .navigationTitle("Archived Questions")
         }
+    }
+    
+    func assignColor() -> String {
+        if(index < 5) {
+            index = 0
+        } else {
+            index += 1
+        }
+        print(colors[index])
+        return colors[index]
     }
 }
 
