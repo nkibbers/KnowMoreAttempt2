1869045f557ca52e6e77288a295b8adc99b4971f
Rudra Parikh <40339080+Rudra1306@users.noreply.github.com>
2024-05-17 11:54:24 +1000

    TriviaCardView Added

---
+++ KnowMoreAttempt2/CardViews/TriviaCardView.swift
@@ -0,0 +1,18 @@
+//
+//  TriviaCardView.swift
+//  KnowMoreAttempt2
+//
+//  Created by Rudra Parikh on 17/5/2024.
+//
+
+import SwiftUI
+
+struct TriviaCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TriviaCardView()
+}
