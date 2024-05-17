220fc20a09f6c29d312df59d2f9886d4f9b73584
Rudra Parikh <40339080+Rudra1306@users.noreply.github.com>
2024-05-17 11:50:06 +1000

    CardDetailModel Added

---
+++ KnowMoreAttempt2/CardViews/CardDetailModel.swift
@@ -0,0 +1,18 @@
+//
+//  CardDetailModel.swift
+//  KnowMoreAttempt2
+//
+//  Created by Rudra Parikh on 17/5/2024.
+//
+
+import SwiftUI
+
+struct CardDetailModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardDetailModel()
+}
