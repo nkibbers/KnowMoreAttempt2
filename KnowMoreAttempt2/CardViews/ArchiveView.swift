a1e8ba9250570886dc65a720567ee426eb1e9ef0
Rudra Parikh <40339080+Rudra1306@users.noreply.github.com>
2024-05-17 11:53:00 +1000

    ArchiveView

---
+++ KnowMoreAttempt2/CardViews/ArchiveView.swift
@@ -0,0 +1,18 @@
+//
+//  ArchiveView.swift
+//  KnowMoreAttempt2
+//
+//  Created by Rudra Parikh on 17/5/2024.
+//
+
+import SwiftUI
+
+struct ArchiveView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ArchiveView()
+}
