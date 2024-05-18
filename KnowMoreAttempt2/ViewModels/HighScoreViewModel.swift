f17d31b168ccbeff7fb4cd6ab01a840b10680059
Rudra Parikh <40339080+Rudra1306@users.noreply.github.com>
2024-05-18 18:28:10 +1000

    Made HighScoreModel

--- KnowMoreAttempt2/ViewModels/HighScoreViewModel.swift
+++ KnowMoreAttempt2/ViewModels/HighScoreViewModel.swift
@@ -7,3 +7,46 @@
 
 import Foundation
 
+import Foundation
+
+class HighScoreViewModel: ObservableObject {
+    @Published var playerScores: [PlayerScoreModel] = []
+    
+    var highScore: Int {
+        playerScores.map { $0.score }.max() ?? 0
+    }
+    
+    var encryptionKey = "savedScores"
+    
+    init() {
+        decodeScores()
+    }
+    
+    //append a score or update a score
+    func saveScore(name: String, score: Int) {
+        if let index = playerScores.firstIndex(where: { $0.name == name && $0.score < score }) {
+            //update the score of an old player in the scoreboard
+            playerScores[index].score = score
+        } else if playerScores.firstIndex(where: { $0.name == name }) == nil {
+            playerScores.append(PlayerScoreModel(name: name, score: score))
+        }
+        encodeScores()
+        decodeScores()
+    }
+    
+    //encoder function
+    func encodeScores() {
+        if let encodedScores = try? JSONEncoder().encode(playerScores) {
+            UserDefaults.standard.set(encodedScores, forKey: encryptionKey)
+            UserDefaults.standard.synchronize()
+        }
+    }
+    
+    //decoder function
+    func decodeScores() {
+        if let savedScores = UserDefaults.standard.data(forKey: encryptionKey),
+           let decodedScores = try? JSONDecoder().decode([PlayerScoreModel].self, from: savedScores) {
+            playerScores = decodedScores
+        }
+    }
+}
